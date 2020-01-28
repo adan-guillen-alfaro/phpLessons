1cd6da98de082c6f70caa9eecc55544fc8fc547f
adan-guillen-alfaro <adan-guillen-alfaro@users.noreply.github.com>
2020-01-28 17:31:34 +0100

    Update login.php

--- AutosDb2/login.php
+++ AutosDb2/login.php
@@ -7,15 +7,26 @@
   }
   if (isset($_POST["name"]) && isset($_POST["pwd"]))
   {
+    $user = $_POST["name"];
     unset($_SESSION["name"]);
-    if (strlen($_POST["name"]) > 0 && strlen($_POST["pwd"]) > 0)
+
+    $_SESSION["lastname"] = $user;
+
+    if (!filter_var($user, FILTER_VALIDATE_EMAIL))
+    {
+      $_SESSION['error'] = "Invalid user name. It must be a valid e-mail.";
+      header("Location: login.php");
+      return;
+    }
+
+    if (strlen($user) > 0 && strlen($_POST["pwd"]) > 0)
     {
         $salt = 'XyZzy12*_';
         $stored_hash = '1a52e17fa899cf40fb04cfc42e6352f1'; // hash for a pwd = 'php123'
         $md5 = hash('md5', $salt.$_POST["pwd"]);
         if ($md5 === $stored_hash)
         {
-          $_SESSION["name"] = $_POST["name"];
+          $_SESSION["name"] = $user;
           unset($_SESSION["error"]);
           header('Location: view.php');
           return;
@@ -56,7 +67,7 @@
           <table>
           <tr>
             <td><label for="name">e-mail: </label></td>
-            <td><input type="text" name="name" id="name"/></td>
+            <td><input type="text" name="name" id="name" value="<?= isset($_SESSION['lastname']) ? $_SESSION['lastname'] : ''; ?>" /></td>
           </tr>
           <tr>
             <td><label for="pwd">Password: </label></td>
