85b3d2eaf3734f2e672a4ffcd4b3e6ff02f06d94
adan-guillen-alfaro <adan-guillen-alfaro@users.noreply.github.com>
2020-01-29 14:01:18 +0100

    register page

--- RinconCrisApp/login.php
+++ RinconCrisApp/login.php
@@ -1,6 +1,12 @@
 <?php
   session_start();
 
+  if (isset($_POST['register']))
+  {
+    header("Location: register.php");
+    return;
+  }
+
   if (isset($_POST["user"]) && isset($_POST["pwd"]))
   {
     unset($_SESSION["activeUser"]);
@@ -15,22 +21,33 @@
     }
     else
     {
-      //TODO
-      $_SESSION["activeUser"] = $_POST["user"];
-      header("Location: main.php");
-      return;
+      checkUser($_POST["user"], $_POST["pwd"]);
+
+      if (isset($_SESSION["activeUser"]))
+      {
+        header("Location: main.php");
+        return;
+      }
+      else
+      {
+        header("Location: login.php");
+        return;
+      }
     }
   }
+
+  function checkUser($user, $pwd)
+  {
+    //TODO
+    $_SESSION["activeUser"] = $user;
+  }
+
  ?>
  <html>
    <head>
        <meta charset="utf-8" />
        <title>El rincón de Cris</title>
-       <style>
-         p.error { display: flex; justify-content: center; align-items: center; color:red; }
-         div.table { display: flex; justify-content: center; align-items: center; }
-         p.headers { display: flex; justify-content: center; align-items: center; }
-       </style>
+       <link rel="stylesheet" href="styles_erc.css">
    </head>
    <body style="font-family: sans-serif;">
     <p class="headers">Bienvenida/o a El rincón de Cris.</p>
@@ -55,7 +72,7 @@
            </tr>
            <tr>
              <td></td>
-             <td><input type="submit" value="LogIn" /></td>
+             <td align="right"><input type="submit" value="LogIn" /><input type="submit" value="Register" name="register" /></td>
            </tr>
          </table>
         </form>
